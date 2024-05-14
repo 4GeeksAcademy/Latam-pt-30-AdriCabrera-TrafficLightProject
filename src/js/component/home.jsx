3b62e1b569cabf6813025120fd1b37278f2b7313
Adriana Cabrera Escobar <160674673+AdriCabrera@users.noreply.github.com>
2024-05-14 00:08:55 +0000

    First commit Traffic Light Project

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [glowColor, setGlowColor] = useState("red")
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<React.Fragment>
+		<div className="traffic-top"></div>
+		<div className="traffic-light">
+			<div onClick={() => setGlowColor("red")} className={"red" + (glowColor === "red" ? " glow" : "")}></div>
+			<div onClick={() => setGlowColor("yellow")} className={"yellow" + (glowColor === "yellow" ? " glow" : "")}></div>
+			<div onClick={() => setGlowColor("green")} className={"green" + (glowColor === "green" ? " glow" : "")}></div>
 		</div>
+		</React.Fragment>
 	);
 };
 
